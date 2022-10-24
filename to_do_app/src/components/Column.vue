71ede9c2bba0889ac4607cf962c7bb2f6dab441e
MARIA <m.g.f@live.com>
2022-10-24 12:44:20 +0200

    recuperado

---
+++ to_do_app/src/components/Column.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+      <header>
+        <h3>{{ name }}</h3>
+      </header>
+      <div>...</div>>
+    </section>
+  </template>
+
+<script>
+
+</script>
---
+++ to_do_app/src/components/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <h3>My Board</h3>
+        <div class="boards-collection"></div>
+        <input
+         type="text"
+         placeholder="Add a board"
+         v-model="boardName"
+         @keyup.enter="add()"
+        />
+        <board-card
+         v-for="(board, index) in boards"
+         :key="index"
+         :name="board.name"
+         :id="board.id">
+        </board-card>
+    </div>
+</template>
+
+<script>
+import BoardCard from '@/components/BoardCard'
+export default {
+    name: 'home-view',
+    components: { BoardCard },
+    data () {
+        return {
+         boardName: '',
+         boards: [
+            { id: 1, name: 'tareas' },
+            { id: 2, name: 'lista de la compra' }
+         ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+  h3 {
+    text-align: left;
+    margin: 1.5rem;
+  }
+
+  .boards-collection {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top: 1rem;
+  }
+
+  input {
+    box-sizing: border-box;
+    background-color: #546E7A;
+    border: 2px solid #546E7A;
+    border-radius: 3px;
+    font-size: 1.1rem;
+    outline: 0;
+    padding: 0.5rem;
+    transition: all 600ms ease;
+  }
+  ::placeholder {
+    color: white;
+   }
+   :focus,:active {
+    background-color: white;
+    color: #546E7A;
+   }
+</style>
